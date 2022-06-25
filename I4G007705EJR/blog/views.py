517a4069d233e03e75b9b692abe3d93bb370e189
furaha <benefatia@gmail.com>
2022-06-25 20:10:29 +0200

    updated CRUD

---
+++ I4G007705EJR/blog/views.py
@@ -0,0 +1,36 @@
+# from dataclasses import field
+# import imp
+# from pyexpat import model
+# from msilib.schema import ListView
+# from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.views.generic.edit import ListView
+from django.views.generic.edit import DetailView
+from django.views.generic.edit import UpdateView
+from .models import Post
+
+
+
+# Create your views here.
+class PostListView(ListView):
+    model = Post
+    
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
+
+class PostDeleteView(UpdateView):
+    model = Post
+    fields = "__all__"
+    success_url  = reverse_lazy("blog:all")
