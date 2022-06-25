85143097b138db226033e27dcefa0675ab20282f
furaha <benefatia@gmail.com>
2022-06-25 21:09:02 +0200

    updated urls.py and views.py

--- I4G007705EJR/blog/views.py
+++ I4G007705EJR/blog/views.py
@@ -5,8 +5,8 @@
 # from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
-from django.views.generic.edit import ListView
-from django.views.generic.edit import DetailView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from django.views.generic.edit import UpdateView
 from .models import Post
 
